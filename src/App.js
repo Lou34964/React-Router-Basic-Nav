a3270b63f03ebc0a82b0d5cc36303a6a1c0350f2
Chad.Snyder <Mapletonkid@gmail.com>
2019-12-16 15:39:35 -0700

    Day 1 MVP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route} from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation />
+    <div><Navigation /></div>
+    
+    {/* <Route exact path="/" exact component = {Home}/>
+
+    <Route path="/About" component = {About}/>
+      
+    
+    <Route path="/Contact" component = {Contact}/> */}
+
+    <Route exact path="/">
+      <Home/>
+    </Route>
+    <Route path="/Home">
+      <Home/>
+    </Route>
+    <Route path ="/About">
+      <Home/>
+    </Route>
   </div>
 );
 
